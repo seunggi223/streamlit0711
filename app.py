cd49be81bc23f4a4b647cd532bcac11a81649c85
seunggi223 <seunggi.alex.hong@gmail.com>
2026-07-11 15:38:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -69,3 +69,7 @@ if st.button("코디 완성하기"):
         with st.expander("코디 연출 팁 영상 보기"):
             st.video("https://www.youtube.com/watch?v=lkMZ8ytly1k")
             st.write("전문가가 제안하는 코디 연출법을 참고해 보세요.")
+
+if st.button("완료"):
+    st.success("성공했습니다!")
+    st.balloons()
