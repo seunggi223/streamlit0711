c5743a4cb1e78a051f450d3dead771ba2075ec43
seunggi223 <seunggi.alex.hong@gmail.com>
2026-07-11 16:28:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -81,9 +81,7 @@ if st.button("코디 완성하기"):
             st.video("https://www.youtube.com/watch?v=lkMZ8ytly1k")
             st.write("전문가가 제안하는 코디 연출법을 참고해 보세요.")
 
-if st.button("전체 초기화"):
-    reset_all()
-
+st.button("전체 초기화", on_click = reset_all)
 
 st.markdown("---")
 
