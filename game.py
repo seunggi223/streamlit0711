6bfe625ad197a700e50f102210e092f0bb86f9b6
seunggi223 <seunggi.alex.hong@gmail.com>
2026-07-11 16:10:21 +0900

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
 st.title("counter app")
-count = 0 
+if 'count' not in st.session_state:
+   st.session_state.count - 0 
+ 
 if st.button("증가"):
-   count += 1
-   st.markdown(f"## 현재 숫자: `{count}`")
+   st.session_state.count += 1
+st.markdown(f"## 현재 숫자: `{st.session_state.count}`")
