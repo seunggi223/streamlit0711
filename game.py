d7b6b492e51cb54fb31d4f27fec9298af79efa54
seunggi223 <seunggi.alex.hong@gmail.com>
2026-07-11 16:10:41 +0900

    Update game.py

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("counter app")
 if 'count' not in st.session_state:
-   st.session_state.count - 0 
+   st.session_state.count = 0 
  
 if st.button("증가"):
    st.session_state.count += 1
