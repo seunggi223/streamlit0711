0ab19c6182586195e90c47347fd3b83ff17547fa
seunggi223 <seunggi.alex.hong@gmail.com>
2026-07-11 15:02:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 import streamlit as st
 
 st.markdown("# 앱 UI 만들기")
-name = st.text_input(
-    "이름", placeholder = "이름"
-)
+name = st.text_input("이름", placeholder = "이름")
 
-grade = st.radio(
-    "학년",["1", "2", "3"]
-)
-class = st.number_input(
-    "반", min_value = 1, max_valume = 10, value = 1
-
-)
+grade = st.radio("학년",["1", "2", "3"])
+class_number = st.number_input("반", min_value = 1, max_valume = 10, value = 1)
 
 
 
